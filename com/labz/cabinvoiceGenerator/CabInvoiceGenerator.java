e416d54c5137025781c4dbca7af57e4b0cc58343
rahul yadav <ry219613@gmail.com>
2021-11-12 22:54:32 +0530

    Added invoice service list

--- com/labz/cabinvoiceGenerator/CabInvoiceGenerator.java
+++ com/labz/cabinvoiceGenerator/CabInvoiceGenerator.java
@@ -12,4 +12,38 @@ public class CabInvoiceGenerator {
 	     }
 	        return totalFare;
 	}
+	 public double calculateFare1(Ride[] rides) {
+	        double totalFare=0;
+	        for (Ride ride:rides){
+	           totalFare += calculateFare(ride.distance,ride.time);
+	        }
+	        return totalFare;
+	    }
+	 public CabInvoiceSummary generateInvoiceForMultipleRides(Ride[] rides, double totalFare) {
+	        for (Ride ride : rides) {
+	            totalFare += calculateFare(ride.distance, ride.time);
+	        }
+	        CabInvoiceSummary summary = new CabInvoiceSummary(totalFare, rides.length, (totalFare / rides.length));
+
+	        return summary;
+	    }
+	    public static double calculateFare(double distance, int time, String string) {
+	        if (string == "normal") {
+	            double fare = 10 * distance + 1 * time;
+	            if (fare < 5) {
+	                return 5;
+	            } else {
+	                return fare;
+	            }
+	        } else {
+	            double fare = 15 * distance + 2 * time;
+	            if (fare < 20) {
+	                return 20;
+	            } else {
+	                return fare;
+	            }
+
+	        }
+
+	    }
 }
