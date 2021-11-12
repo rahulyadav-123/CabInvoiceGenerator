3d79ed522f47cd72199323c72ce1f0e6debb3546
rahul yadav <ry219613@gmail.com>
2021-11-12 22:30:27 +0530

    tested total fare for journey

--- com/labz/cabinvoiceGenerator/CabInvoiceGenerator.java
+++ com/labz/cabinvoiceGenerator/CabInvoiceGenerator.java
@@ -1,5 +1,15 @@
 package com.labz.cabinvoiceGenerator;
 
 public class CabInvoiceGenerator {
+	 private static final double costPerKm = 10.0;
+	 private static final double costPerMinute = 1;
+	 private static final double minFare = 5;
 
+	 public double calculateFare(double distance, int time) {
+		 double totalFare= ((distance *costPerKm) + (time * costPerMinute));
+	     if (totalFare<minFare){
+	    	 return  minFare;
+	     }
+	        return totalFare;
+	}
 }
